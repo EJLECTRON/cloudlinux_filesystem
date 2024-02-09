0b30ce39a821b90c449ab5d1fef5b490d3799b9d
ejlectron <ishchenko_nikolay@knu.ua>
2024-02-09 12:30:04 +0100

    Added --version, --help options

--- lnxmanager/cli.py
+++ lnxmanager/cli.py
@@ -10,7 +10,6 @@ def _version_callback(value: bool) -> None:
         typer.echo(f"{__app_name__} v{__version__}")
         raise typer.Exit()
 
-
 @app.callback()
 def main(
     version: Optional[bool] = typer.Option(
